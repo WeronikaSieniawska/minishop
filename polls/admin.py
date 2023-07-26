53f3649b10d6b053d57108e9bdc76577c11d9cca
WeronikaSieniawska <84450078+WeronikaSieniawska@users.noreply.github.com>
2023-07-26 16:43:13 +0000

    first try1

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-class ChoiceInline(admin.TabularInline):
+class InvoceOfSales(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -12,7 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
-    inlines = [ChoiceInline]
+    inlines = [InvoceOfSales]
     list_display = ["question_text", "pub_date", "was_published_recently"]
     search_fields = ["question_text"]
 
